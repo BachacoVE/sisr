2a178297e03ccf7fa020f14152ef53ac5e728966
Victor Inojosa <vinojosa@inces.gob.ve>
2015-10-28 10:20:31 -0430

    se resuelve el inner join m2m hacia for_pis_maestros

--- consultas_sql/generacion_contratos.sql
+++ consultas_sql/generacion_contratos.sql
@@ -1,15 +1,4 @@
-﻿--﻿/*QUERY REALIZADO PARA LA GENERACIÓN CONTRATOS*/
-
-
-
-/*DROP TABLE IF EXISTS tmp_fac_3004;
-
-CREATE TEMP TABLE tmp_fac_3004 AS (select distinct facilitador_id 
-                                 from for_pis_registro_proyeccion
-                                 where fecha_cierre <= '2015-04-30');
-                                 
-*/                                   
-SELECT  mae.nombres, mae.apellidos, mae.cedula, nac.nacionalidad, civ.estado_civil,
+﻿SELECT  mae.nombres, mae.apellidos, mae.cedula, nac.nacionalidad, civ.estado_civil,
         mae.direccion_habitacion, mun.municipio, es.estado,
         --to_char (min(pry.fecha_inicio), 'DD/MM/YYYY') as primera_fecha_inicio,
         --to_char (max(pry.fecha_cierre), 'DD/MM/YYYY') as ultima_fecha_cierre,
@@ -23,15 +12,23 @@ SELECT  mae.nombres, mae.apellidos, mae.cedula, nac.nacionalidad, civ.estado_civ
     
 FROM  for_pis_registro_inicial as pry
 --INNER JOIN  tmp_fac_3004 tmp ON pry.facilitador_id = tmp.facilitador_id
+-- INNER JOIN  for_pis_maestros mae (QUITAR)
+--  ON pry.facilitador_id = mae.id (QUITAR)
+
+INNER JOIN for_pis_mae_participacion_pis mae_part
+  ON pry.id = mae_part.numero_id
+
+INNER JOIN for_pis_maestros as mae
+  ON mae_part.maestro_id = mae.id
+
 INNER JOIN for_pis_estados es
   ON mae.estado_id = es.id
-INNER JOIN  for_pis_maestros mae
-  ON pry.facilitador_id = mae.id
+
 INNER JOIN  for_pis_municipios mun
   ON mae.municipio_id = mun.id
 LEFT OUTER JOIN  for_pis_mae_valor_hora vh
   ON mae.nivel_id = vh.id
-INNER JOIN  for_pis_cfs_extended cfs
+INNER JOIN  for_pis_cfs cfs
   ON pry.cfs_id = cfs.id
 INNER JOIN  for_pis_nacionalidades nac
   ON mae.nacionalidad = nac.id
@@ -49,11 +46,12 @@ WHERE mae.condicion_laboral in (3)
   AND nac.nacionalidad is not null
   AND civ.estado_civil is not null
   AND mae.direccion_habitacion is not null
-  AND mae.municipio is not null
-  AND mae.estado is not null
+  AND mae.municipio_id is not null
+  AND mae.estado_id is not null
   AND cfs.nombre is not null
   AND vh.valor_hora is not null 
-  AND lab.condicion_laboral is not null
+  AND mae.condicion_laboral is not null
+  AND mae.apr_generar is false
 --GROUP BY 1,2,3,10,11
 GROUP BY 1,2,3,4,5,6,7,8,12,15,16
 ORDER BY es.estado, mae.cedula
