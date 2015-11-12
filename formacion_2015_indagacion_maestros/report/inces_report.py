807c9e410ed0aa1f38932d4d850ced8a28e4c1c0
root <root@cesar.inces>
2015-11-12 09:24:49 -0430

    se agregan validaciones al py de contratos y se corrige la longitud de los campos en la foto que se guarda en la clase contratos

--- formacion_2015_indagacion_maestros/report/inces_report.py
+++ formacion_2015_indagacion_maestros/report/inces_report.py
@@ -3,6 +3,7 @@ import datetime
 from numero_a_texto import Numero_a_Texto
 from convertir import convertir#agregar para RML
 from report import report_sxw
+from osv import fields,osv
 
 class reporte_inces(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context=None):
@@ -19,7 +20,7 @@ class reporte_inces(report_sxw.rml_parse):
             'max_fecha':self.max_fecha,
             'determinar_valor_hora':self.determinar_valor_hora,
             'marcar_contrato_generado':self.marcar_contrato_generado,
-            'agregar_a_clase_contratos':self.agregar_a_clase_contratos
+            'agregar_a_clase_contratos':self.agregar_a_clase_contratos,
         })
 
     def obt_texto(self,cantidad):
@@ -60,6 +61,8 @@ class reporte_inces(report_sxw.rml_parse):
                         ON for_pis_registro_inicial.id=for_pis_mae_participacion_pis.numero_id  \
                         WHERE for_pis_maestros.cedula='%s'" % cedula)
         fec_minima=self.cr.fetchone()
+        if(fec_minima<0):
+            fec_minima='2015-05-01'
 
         if(fec_minima > '2015-05-01'):
             self.cr.execute("SELECT valor_hora \
@@ -68,7 +71,10 @@ class reporte_inces(report_sxw.rml_parse):
                         ON for_pis_maestros.nivel_id=for_pis_mae_valor_hora.id \
                         WHERE for_pis_maestros.cedula='%s'" % (cedula))
             resultado1=self.cr.fetchone()
-            return resultado1[0]
+            if(resultado1<0):
+                return 0
+            else:
+                return resultado1[0]
         else:
             self.cr.execute("SELECT valor_hora \
                         FROM for_pis_maestros \
@@ -83,12 +89,18 @@ class reporte_inces(report_sxw.rml_parse):
                         ON for_pis_maestros.nivel_id=for_pis_mae_valor_hora.id \
                         WHERE for_pis_maestros.cedula='%s'" % (cedula))
                 resultado1=self.cr.fetchone()
-                return resultado1[0]
+                if(resultado1<0):
+                    return 0
+                else:
+                    return resultado1[0]
             else:
                 return resultado2[0]
 
     def marcar_contrato_generado(self, cedula):
-        self.cr.execute("UPDATE for_pis_maestros SET apr_generar=%s WHERE cedula=%s",(True,cedula))
+        self.cr.execute("SELECT apr_generar FROM for_pis_maestros WHERE cedula='%s'" % (cedula))
+        booleano=self.cr.fetchone()
+        if(booleano[0]==None or booleano[0]==False):
+            self.cr.execute("UPDATE for_pis_maestros SET apr_generar='%s' WHERE cedula='%s'" % (True,cedula))
 
     def agregar_a_clase_contratos(self, cedula, nombres, apellidos, nacionalidad, estado_civil, domicilio, municipio, estado, fecha_inicio, fecha_cierre, cfs, total_horas, valor_hora, condicion_laboral):
         fecha_generado=time.strftime("%d-%m-%Y")
@@ -104,6 +116,8 @@ class reporte_inces(report_sxw.rml_parse):
                         ON for_pis_maestros.id=for_pis_mae_participacion_pis.maestro_id \
                         WHERE for_pis_maestros.cedula='%s'" % cedula)
         total_formaciones=self.cr.fetchone()
+        if(total_formaciones<0):
+            total_formaciones=0
 
         self.cr.execute("SELECT for_dependencias.dependencia \
                         FROM dependencia_formador_rel \
@@ -113,6 +127,50 @@ class reporte_inces(report_sxw.rml_parse):
                         ON for_dependencias.id=dependencia_formador_rel.dependencia_id \
                         WHERE for_pis_maestros.cedula='%s'" % (cedula))
         dependencia=self.cr.fetchone()
+        if(dependencia<0):
+            dependencia = "CAMPO VACIO" 
+
+        if(cedula<0):
+            cedula=0
+
+        if(nombres<0):
+            nombres="SIN NOMBRES"
+
+        if(apellidos<0):
+            apellidos="SIN APELLIDOS"
+
+        if(nacionalidad<0):
+            nacionalidad="SIN NACIONALIDAD"
+
+        if(estado_civil<0):
+            estado_civil="SIN ESTADO CIVIL"
+
+        if(domicilio<0):
+            domicilio="SIN DOMICILIO"
+
+        if(municipio<0):
+            municipio="SIN MUNICIPIO"
+
+        if(estado<0):
+            estado="SIN ESTADO"
+
+        if(fecha_inicio<'0000-00-00'):
+            fecha_inicio='0000-00-00'
+
+        if(fecha_cierre<'0000-00-00'):
+            fecha_cierre='0000-00-00'
+
+        if(cfs==''):
+            cfs="SIN CFS"
+
+        if(total_horas<0):
+            total_horas=0
+
+        if(valor_hora<0):
+            valor_hora=0
+
+        if(condicion_laboral<0):
+            condicion_laboral="SIN CONDICION LABORAL"
 
         self.cr.execute("INSERT INTO for_pis_contratos (create_uid, create_date, cedula, nombres, apellidos, nacionalidad, estado_civil, domicilio, municipio, estado, fecha_inicio, fecha_cierre, cfs, total_horas, valor_hora, fecha_generado, total_pagar, condicion_laboral, active, total_formaciones, dependencia) \
                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (create_uid,create_date,cedula, nombres, apellidos, nacionalidad, estado_civil, domicilio, municipio, estado, fecha_inicio, fecha_cierre, cfs, total_horas, valor_hora, fecha_generado, total_pagar, condicion_laboral, active, total_formaciones, dependencia))
